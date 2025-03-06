bf972fbf12530705195dabf155faa136674e5337
w33bvGL <w33bv.gl@gmail.com>
2025-03-06 15:00:29 +0400

    fix rewrite files

--- nuxt-ui-pro-crack/rewriteFiles.js
+++ nuxt-ui-pro-crack/rewriteFiles.js
@@ -1,23 +1,26 @@
 import fs from 'fs-extra';
-import { resolve, join } from 'path';
-
-const __dirname = dirname(new URL(import.meta.url).pathname);
+import { resolve, join, dirname } from 'path';
 
 const uiProDir = resolve('node_modules', '@nuxt', 'ui-pro');
 
-const licenseSourceFilePath = resolve(__dirname, 'modules', 'pro', 'license.ts');
+const licenseSourceFilePath = resolve('nuxt-ui-pro-crack', 'modules', 'pro', 'license.ts');
 
 async function rewriteFiles() {
     try {
         if (fs.existsSync(uiProDir)) {
+            console.log(licenseSourceFilePath);
             console.log(`Found ui-pro in node_modules. Rewriting files...`);
 
             const filePath = join(uiProDir, 'modules', 'pro', 'license.ts');
+            console.log(filePath);
+            const licenseContent = await fs.readFile(licenseSourceFilePath, 'utf-8');
 
-            await fs.outputFile(filePath, licenseSourceFilePath);
+            // Записываем содержимое в целевой файл
+            await fs.outputFile(filePath, licenseContent);
+
             console.log('Файл успешно перезаписан!');
         } else {
-            console.log('ui-pro не найден в node_modules' + uiProDir);
+            console.log('ui-pro не найден в node_modules: ' + uiProDir);
         }
     } catch (err) {
         console.error('Ошибка при перезаписи файлов:', err);

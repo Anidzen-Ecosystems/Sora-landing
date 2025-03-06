bea8427f7afa0c20252173d09d03e30cc27ba396
w33bvGL <w33bv.gl@gmail.com>
2025-03-06 19:37:01 +0400

    add overview

---
+++ components/.bin/overview.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const { t } = useI18n();
+const { scrollToSection } = useScrollToSection();
+
+import { GET_ANIME_QUERY } from '~/graphql/getAnimeQueryOne';
+
+const graphqlCode = GET_ANIME_QUERY;
+</script>
+<template>
+    <section id="overview" class="section mb-10">
+        <UContainer>
+            <div class="flex gap-5">
+
+                <div class="w-1/2">
+                    <div class="mb-5">
+                        <UPageHero
+                            title="Official Support"
+                            description="Through NuxtLabs experts and agency networks, we have a solution for every need."
+                        />
+                        <h3 class="text-4xl font-semibold text-primary">
+                            {{ t('overview.title') }}
+                        </h3>
+                        <p class="mt-2">
+                            {{ t('overview.description') }}
+                        </p>
+                        <p class="mt-4">
+                            {{ t('overview.no-commerce') }}
+                        </p>
+                        <div class="mt-4 flex gap-3">
+                            <UButton
+                                size="xl"
+                                icon="i-heroicons-key"
+                                @click="scrollToSection('#call-to-action')"
+                            >
+                                {{ t('overview.cta.buttonGetAPI') }}
+                            </UButton>
+                            <UButton
+                                size="xl"
+                                variant="soft"
+                                target="_blank"
+                                to="https://myanimelist.net"
+                            >
+                                {{ t('overview.cta.buttonMAL') }}
+                            </UButton>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <UiCodeBlock
+                        :code="graphqlCode"
+                        language="graphql"
+                        background-color="#212a3b"
+                        show-mac-header
+                        show-copy-button
+                    />
+                </div>
+            </div>
+        </UContainer>
+    </section>
+</template>
--- components/overview.vue
+++ components/overview.vue
@@ -1,55 +1,43 @@
 <script setup lang="ts">
+import CodeBlock from '~/components/ui/codeBlock.vue';
+
 const { t } = useI18n();
 const { scrollToSection } = useScrollToSection();
 
 import { GET_ANIME_QUERY } from '~/graphql/getAnimeQueryOne';
 
 const graphqlCode = GET_ANIME_QUERY;
+
+const items = [
+    { label: t('cta.button_text'), size: 'xl', variant: 'soft' },
+    {
+        label: t('cta.my_anime_list'),
+        variant: 'link',
+        color: 'gray',
+        size: 'md',
+        trailingIcon: 'i-heroicons-arrow-right-20-solid',
+        to: 'https://myanimelist.net',
+        target: '_blank',
+    },
+];
 </script>
 <template>
     <section id="overview" class="section mb-10">
         <UContainer>
-            <div class="flex gap-5">
-                <div class="w-1/2">
-                    <div class="mb-5">
-                        <h3 class="text-4xl font-semibold text-primary">
-                            {{ t('overview.title') }}
-                        </h3>
-                        <p class="mt-2">
-                            {{ t('overview.description') }}
-                        </p>
-                        <p class="mt-4">
-                            {{ t('overview.no-commerce') }}
-                        </p>
-                        <div class="mt-4 flex gap-3">
-                            <UButton
-                                size="xl"
-                                icon="i-heroicons-key"
-                                @click="scrollToSection('#call-to-action')"
-                            >
-                                {{ t('overview.cta.buttonGetAPI') }}
-                            </UButton>
-                            <UButton
-                                size="xl"
-                                variant="soft"
-                                target="_blank"
-                                to="https://myanimelist.net"
-                            >
-                                {{ t('overview.cta.buttonMAL') }}
-                            </UButton>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-1/2">
-                    <UiCodeBlock
-                        :code="graphqlCode"
-                        language="graphql"
-                        background-color="#212a3b"
-                        show-mac-header
-                        show-copy-button
-                    />
-                </div>
-            </div>
+            <ULandingSection
+                :title="t('overview.title')"
+                :description="t('overview.description')"
+                :links="items"
+                align="left"
+            >
+                <CodeBlock
+                    :code="graphqlCode"
+                    language="graphql"
+                    background-color="#212121"
+                    show-mac-header
+                    show-copy-button
+                />
+            </ULandingSection>
         </UContainer>
     </section>
 </template>

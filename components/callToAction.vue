ccf78ef2be075ff0343c2404b2ff542f45fdd36f
w33bvGL <w33bv.gl@gmail.com>
2025-03-06 17:52:19 +0400

    fix call to action component

--- components/callToAction.vue
+++ components/callToAction.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 const { t } = useI18n();
 
-
-const links = computed(() => [
-    { label: t('cta.button_text'), size: 'xl', variant: 'soft' },
+const items = computed(() => [
+    { label: t('cta.button_text'), size: 'xl', variant: 'soft'},
+    { label: t('cta.my_anime_list'), variant: 'link', color: 'gray', size: 'md', trailingIcon: 'i-heroicons-arrow-right-20-solid', to: 'https://myanimelist.net', target: '_blank' }
 ]);
 </script>
 
 <template>
     <section id="call-to-action" class="section mb-10">
         <UContainer>
-            <ULandingCTA :title="t('cta.title')" :description="t('cta.description')" :links="links"/>
+            <ULandingCTA :title="t('cta.title')" :description="t('cta.description')" :items="items"/>
         </UContainer>
     </section>
 </template>
